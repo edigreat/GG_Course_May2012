8d81c3f1146138810e7254ec1801ae9a229e17b5
Heriberto G <heriberto.galdamez@gmail.com>
2012-05-28 18:21:47 -0500

    se agrega funcionalidad para presentar ventana modal para confirmar shopping cart

--- edigreat/Amazeet/web-app/js/application.js
+++ edigreat/Amazeet/web-app/js/application.js
@@ -7,3 +7,81 @@ if (typeof jQuery !== 'undefined') {
 		});
 	})(jQuery);
 }
+
+$(document).ready(function(){
+        var ancho = 600;
+        var alto = 250;
+       $(document).on("click", "#button", function(){ 
+      //  $('#button').click(function(){
+
+       $.ajax({
+                  type: "POST",
+                  url: "/Amazeet/store/showShoppingCart",
+                  success: function(datos){
+                  var contenidoHTML = '';
+                  contenidoHTML = contenidoHTML + datos;
+                  var bgdiv        = $('<div>').attr({
+                                        class: 'bgtransparent',
+                                        id: 'bgtransparent'
+                                        });
+              
+                $(bgdiv).fadeIn(1000);
+                $(bgdiv).fadeTo("slow",0.6);
+
+                $('body').append(bgdiv);
+
+                var wscr = $(window).width();
+                var hscr = $(window).height();
+
+                $('#bgtransparent').css("width", wscr);
+                $('#bgtransparent').css("height", hscr);
+
+                var moddiv = $('<div>').attr({
+                                        class: 'bgmodal',
+                                        id: 'bgmodal'
+                                        });
+
+                 $(moddiv).fadeIn(1500);
+
+                 
+                $('body').append(moddiv);
+
+                $('#bgmodal').append(contenidoHTML);
+
+                $(window).resize();
+            
+                }
+            });  
+
+
+        });
+
+        $(window).resize(function(){
+                var wscr = $(window).width();
+                var hscr = $(window).height();
+
+                $('#bgtransparent').css("width", wscr);
+                $('#bgtransparent').css("height", hscr);
+
+                $('#bgmodal').css("width", ancho+'px');
+                $('#bgmodal').css("height", alto+'px');
+
+                var wcnt = $('#bgmodal').width();
+                var hcnt = $('#bgmodal').height();
+
+                var mleft = ( wscr - wcnt ) / 2;
+                var mtop = ( hscr - hcnt ) / 2;
+
+                $('#bgmodal').css("left", mleft+'px');
+                $('#bgmodal').css("top", mtop+'px');
+
+
+        });
+
+ });
+
+function closeModal(){
+
+        $('#bgmodal').remove();
+        $('#bgtransparent').remove();
+}
