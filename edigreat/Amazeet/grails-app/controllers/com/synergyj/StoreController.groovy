3f75fd5aacbc2e2c1d638c9bbb1bb43c995bc46b
Heriberto G <heriberto.galdamez@gmail.com>
2012-05-27 19:14:36 -0500

    se agrega metodo addItemAsync

--- edigreat/Amazeet/grails-app/controllers/com/synergyj/StoreController.groovy
+++ edigreat/Amazeet/grails-app/controllers/com/synergyj/StoreController.groovy
@@ -3,9 +3,9 @@ package com.synergyj
 class StoreController {
 
 	//Se ejecuta antes de cada accion
-	
+
 	def beforeInterceptor = {
-		
+
 		log.debug "Estoy en la accion ${actionName} del controlador ${controllerName} [${session.shoppingCart}]"
 
 		if(!session.shoppingCart)
@@ -14,40 +14,40 @@ class StoreController {
 			def shoppingCart =  new ShoppingCart(shoppingCartStatus:ShoppingCartStatus.SHOPPING)
 			shoppingCart.items = []
 			session.shoppingCart =shoppingCart
-			
+
 		}
-		
+
 		log.debug "Despues en la accion ${actionName} del controlador ${controllerName} [${session.shoppingCart}]"
-		
-		
+
+
 	}
-	
+
     def index() {
 		//TODO: Catalogo de productos
 		def products = Product.list()
 		//Catalogo de categorias
 		//def categories = Category.list()
 		//Todas las acciones regresan un mapa
-		
+
 		[products:products]
-		
+
 		}
-	
+
 	def reviewItem = {
 		//TODO: Obtener el id de producto y mostrar el detalle de los productos
 		//log.debug "Params ${params.id}"
 		redirect(controller:'product',action:'show',id:params.id)
-		
+
 		}
-	
+
 	def addItem() {
 		def product = Product.get(params.productId)
 		def item = new ItemToPurchase(product:product,quantity:params.int('quantity'))
 		session.shoppingCart.items << item
 		log.debug item.dump()
-		
+
 		//Render puede recibir el objeto modelo  como un mapa y se lo pega a la vista
-		
+
 		//log.debug request.dump()
 		//log.debug response.dump()
 //		log.debug session
@@ -55,16 +55,40 @@ class StoreController {
 	//	log.debug flash.dump()
 		flash.message = "Haz agregado un producto a tu carrito"
 		redirect action:'index'
-		
+
 		}
-	
-	
+
+
 	def addItemAsync(){
-		
-		log.debug "Hola mundo ajax"
-		
-		render "Hola mundo ajax"
+
+		def product = Product.get(params.productId)
+	    boolean isItem = false
+
+	    session.totalCompra = 0
+	    //Buscamos si el articulo ya se encuentra en shoppingCart, EqualsAndHashCode en Product
+		session.shoppingCart.items.each{ it ->
+
+                if(it.product == product)
+                {
+                  it.quantity =  it.quantity + params.int('quantity')
+                  isItem = true
+                }
+
+                session.totalCompra =session.totalCompra + it.product.price * it.quantity
 		}
-	
-	
+
+        //si no se encuentra lo agregamos
+        if(!isItem)
+	    {
+	    	def item = new ItemToPurchase(product:product,quantity:params.int('quantity'))
+            session.shoppingCart.items << item
+            session.totalCompra =session.totalCompra + item.product.price * item.quantity
+        }
+
+		render(template: "/store/myCart")
+
+		}
+
+
 }
+
